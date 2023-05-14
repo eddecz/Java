1176821ea1385f439c6c8cc022ff6caddbc525c1
eddecz <133551650+eddecz@users.noreply.github.com>
2023-05-14 19:43:34 -0300

    Create 002-ExemploDeLoopWhile.java

---
+++ Livro-UseACabeça/testes/002-ExemploDeLoopWhile.java
@@ -0,0 +1,18 @@
+public class Looping {
+  public static void main(String[] args) {
+  int x = 1;
+    
+  while (x < 6) {
+  System.out.println("Tropa do leleu");
+    x = x+1;
+    }
+  }
+}
+
+/*Foi criada uma variavel do tipo inteiro (x) com o valor 1*/
+
+/*As condições para o looping funcionar era que x precisava
+  ser menor que 6 (x < 6), enquanto essa condição fosse verdadeira
+  retornaria uma saida (System.out.println("Tropa do leleu"))*/
+
+/*Uma soma é realizada até a condição se tornar falsa (x = x + 1)*/
