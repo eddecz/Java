de6829b6cbb65bd0dc1d3ed497038902ea24c06a
eddecz <133551650+eddecz@users.noreply.github.com>
2023-05-14 18:16:49 -0300

    Update and rename

---
+++ testes/001-AnatomiaDeUmaClasse.java
@@ -0,0 +1,9 @@
+public class Anatomia { 
+  public static void main (String[] args){ 
+    }
+  }
+/* crio uma classe (class) publica (public) que recebe o nome de 
+   Anatomia e abro a classe com uma chave*/
+/*crio um método principal (main) publico (public) com um tipo de 
+  retorno (void - não há valor de retorno) usando o argumento do 
+  método (recebe uma matriz de String, e a matriz se chamará 'args')*/
